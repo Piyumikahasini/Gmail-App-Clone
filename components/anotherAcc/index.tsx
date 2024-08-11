8c44a5917befd9febb1f25947cd6a9b1529abfd1
Piyumikahasini <hasinipiyumika617@gmail.com.com>
2024-08-11 17:33:12 +0530

    update

--- components/anotherAcc/index.tsx
+++ components/anotherAcc/index.tsx
@@ -10,8 +10,8 @@ export default function AnotherAcc(){
         <View style={{marginBottom:-10,zIndex:99999999999999}}>
         <Image source={{ uri: "https://cdn-icons-png.freepik.com/512/8345/8345328.png" }}style={{ width: 43, height: 43, borderRadius: 17.5,marginTop:32,alignSelf:'flex-start',left:20 }}/>
        <View style={{position:'absolute',left:85,top:35,width:viewWidth,height:viewHeight}}>
-         <Text>Dinil Damsith</Text>
-         <Text>dinildamsith52@gmail.com</Text>
+         <Text>Chathurangi</Text>
+         <Text>chathurangi@gmail.com</Text>
          <Text style={{position:'absolute',right:110,top:22}}>40</Text>
        </View>
         </View>
