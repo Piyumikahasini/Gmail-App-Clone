3250d993d6731375c49b92096fe0aca29e9ef280
Piyumikahasini <hasinipiyumika617@gmail.com.com>
2024-08-11 17:38:52 +0530

    update

--- components/searchBar/index.tsx
+++ components/searchBar/index.tsx
@@ -69,7 +69,7 @@ export default function SearchBar(){
        </View>
        <View style={{position:'absolute',left:85,top:38,width:viewWidth,height:viewHeight}}>
          <Text>Chathurangi</Text>
-         <Text>dinildamsith52@gmail.com</Text>
+         <Text>chathurangi@gmail.com</Text>
          <Text style={{position:'absolute',right:110,top:22}}>40</Text>
          <Pressable style={{backgroundColor:'white',width:220,height:40,borderRadius:10,marginTop:20,borderWidth:1,borderColor:'black'}}>
            <Text style={{marginTop:8,alignSelf:'center'}}>Manage Your Google Account</Text>
